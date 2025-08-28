13380ec095a6b484c02d8e6047e48cc12b297fa0
AscendedGravity <dspradau@gmail.com>
2025-08-28 09:18:56 -0500

    feat: add player management and SSE support for player events

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use std::{net::SocketAddr, path::Path, sync::Arc};
+use std::{net::SocketAddr, path::Path, sync::Arc, collections::HashMap};
 use bindings::sdk::{DbConnectionBuilder, __codegen::SpacetimeModule};
 use anyhow::{anyhow, Result};
 use intmap::IntMap;
@@ -42,18 +42,22 @@ pub struct AppConfig {
     pub server: ServerConfig,
     pub resources: Vec<Entity>,
     pub enemies: Vec<Entity>,
+    pub players: Vec<Entity>,
 }
 
 pub struct EntityGroup {
     pub nodes: RwLock<IntMap<u64, [i32; 2]>>,
+    pub player_names: RwLock<HashMap<u64, String>>, // Track player names by entity_id
     pub properties: Value,
 }
 
 pub struct AppState {
     pub resource: IntMap<i32, EntityGroup>,
     pub enemy: IntMap<i32, EntityGroup>,
+    pub player: IntMap<i32, EntityGroup>,
     pub resources_list: Vec<Entity>,
     pub enemies_list: Vec<Entity>,
+    pub players_list: Vec<Entity>,
 }
 
 
@@ -91,13 +95,19 @@ impl AppConfig {
         let mut state = AppState {
             resource: IntMap::with_capacity(self.resources.len()),
             enemy: IntMap::with_capacity(self.enemies.len()),
+            player: IntMap::with_capacity(self.players.len()),
             resources_list: self.resources.clone(),
             enemies_list: self.enemies.clone(),
+            players_list: self.players.clone(),
         };
 
         sub.push_group(String::from("resources: "));
         for Entity { id, name: _, properties } in self.resources {
-            state.resource.insert(id, EntityGroup { nodes: RwLock::new(IntMap::new()), properties });
+            state.resource.insert(id, EntityGroup { 
+                nodes: RwLock::new(IntMap::new()), 
+                player_names: RwLock::new(HashMap::new()),
+                properties 
+            });
 
             sub.push_query(move || vec![
                 format!("SELECT res.* FROM resource_state res JOIN location_state loc ON res.entity_id = loc.entity_id WHERE res.resource_id = {};", id),
@@ -107,11 +117,31 @@ impl AppConfig {
 
         sub.push_group(String::from("enemies: "));
         for Entity { id, name: _, properties } in self.enemies {
-            state.enemy.insert(id, EntityGroup { nodes: RwLock::new(IntMap::new()), properties });
+            state.enemy.insert(id, EntityGroup { 
+                nodes: RwLock::new(IntMap::new()), 
+                player_names: RwLock::new(HashMap::new()),
+                properties 
+            });
         }
         sub.push_query(move || vec![
             String::from("SELECT mob.* FROM enemy_state mob JOIN mobile_entity_state loc ON mob.entity_id = loc.entity_id;"),
             String::from("SELECT loc.* FROM mobile_entity_state loc JOIN enemy_state mob ON loc.entity_id = mob.entity_id;"),
+        ]);
+
+        sub.push_group(String::from("players: "));
+        for Entity { id, name: _, properties } in self.players {
+            state.player.insert(id, EntityGroup { 
+                nodes: RwLock::new(IntMap::new()), 
+                player_names: RwLock::new(HashMap::new()),
+                properties 
+            });
+        }
+        // Use separate subscriptions to avoid race conditions with JOINs
+        // Include player_username_state for player names
+        sub.push_query(move || vec![
+            String::from("SELECT loc.* FROM mobile_entity_state loc;"),
+            String::from("SELECT sip.* FROM signed_in_player_state sip;"),
+            String::from("SELECT pus.* FROM player_username_state pus;"),
         ]);
 
         (Arc::new(state), self.db, sub, self.server)
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use crate::{channels::*, config::*, subscription::*};
 
 use std::io::{stdout, Write};
 use std::sync::Arc;
+use std::collections::HashSet;
+use tokio::sync::RwLock as TokioRwLock;
 use bindings::{sdk::DbContext, region::*};
+
 use axum::{
     Router, Json, 
     routing::get, 
@@ -31,6 +34,14 @@ enum Message {
 
     EnemyInsert { id: u64, mob: i32, x: i32, z: i32 },
     EnemyDelete { id: u64, mob: i32 },
+
+    PlayerInsert { id: u64, char: i32, x: i32, z: i32 },
+    PlayerDelete { id: u64, char: i32 },
+    
+    PlayerSignIn { id: u64, username: Option<String> },
+    PlayerSignOut { id: u64 },
+    
+    PlayerNameUpdate { id: u64, username: String },
 }
 
 #[derive(Clone)]
@@ -41,6 +52,7 @@ struct SseEvent {
 struct AppStateWithSse {
     app_state: Arc<AppState>,
     sse_tx: broadcast::Sender<SseEvent>,
+    signed_in_players: Arc<TokioRwLock<HashSet<u64>>>,
 }
 
 impl Message {
@@ -58,6 +70,14 @@ impl Message {
 
     pub fn enemy_delete(mob: &EnemyState) -> Self {
         Self::EnemyDelete { id: mob.entity_id, mob: mob.enemy_type as i32 }
+    }
+
+    pub fn player_insert(entity_id: u64, loc: &MobileEntityState) -> Self {
+        Self::PlayerInsert { id: entity_id, char: 1, x: loc.location_x, z: loc.location_z }
+    }
+
+    pub fn player_delete(entity_id: u64) -> Self {
+        Self::PlayerDelete { id: entity_id, char: 1 }
     }
 }
 
@@ -90,9 +110,11 @@ async fn main() {
 
     // Create SSE broadcast channel
     let (sse_tx, _) = broadcast::channel(1000);
+    let signed_in_players = Arc::new(TokioRwLock::new(HashSet::new()));
     let app_state_with_sse = AppStateWithSse {
         app_state: state.clone(),
         sse_tx: sse_tx.clone(),
+        signed_in_players: signed_in_players.clone(),
     };
 
     let (tx, rx) = unbounded_channel();
@@ -124,17 +146,61 @@ async fn main() {
         Some(Message::enemy_delete(row))
     );
 
-    con.db.mobile_entity_state().on_update_send(&tx, |ctx, _, new|
-        ctx.db.enemy_state()
+    // Player event handlers - track signed-in players using message passing
+    con.db.signed_in_player_state().on_insert_send(&tx, |ctx, row| {
+        // Look up the player's username when they sign in
+        let username = ctx.db.player_username_state()
             .entity_id()
-            .find(&new.entity_id)
-            .map(|mob| Message::enemy_insert(&mob, new))
-    );
+            .find(&row.entity_id)
+            .map(|player_username| player_username.username);
+        
+        Some(Message::PlayerSignIn { id: row.entity_id, username })
+    });
+    
+    con.db.signed_in_player_state().on_delete_send(&tx, |_, row| {
+        Some(Message::PlayerSignOut { id: row.entity_id })
+    });
+    
+    // Track player name changes
+    con.db.player_username_state().on_insert_send(&tx, |_, row| {
+        Some(Message::PlayerNameUpdate { id: row.entity_id, username: row.username.clone() })
+    });
+    
+    con.db.player_username_state().on_update_send(&tx, |_, _, new_row| {
+        Some(Message::PlayerNameUpdate { id: new_row.entity_id, username: new_row.username.clone() })
+    });
+    
+    // Comprehensive mobile entity handler to distinguish between enemies and potential players
+    con.db.mobile_entity_state().on_update_send(&tx, {
+        let tx_clone = tx.clone();
+        move |ctx, _, new| {
+            if let Some(mob) = ctx.db.enemy_state().entity_id().find(&new.entity_id) {
+                // This is an enemy
+                Some(Message::enemy_insert(&mob, new))
+            } else {
+                // This could be a player - we'll filter by signed-in status in consume()
+                // Also try to get the username if this is a signed-in player
+                if ctx.db.signed_in_player_state().entity_id().find(&new.entity_id).is_some() {
+                    // This is a signed-in player, get their username
+                    let username = ctx.db.player_username_state()
+                        .entity_id()
+                        .find(&new.entity_id)
+                        .map(|player_username| player_username.username)
+                        .unwrap_or_else(|| format!("Player_{}", new.entity_id));
+                    
+                    // Send name update first
+                    let _ = tx_clone.send(Message::PlayerNameUpdate { id: new.entity_id, username });
+                }
+                
+                Some(Message::player_insert(new.entity_id, &new))
+            }
+        }
+    });
 
     let (tx_sig, rx_sig) = oneshot::channel();
 
     let mut producer = Box::pin(con.run_async());
-    let consumer = tokio::spawn(consume(rx, state.clone(), sse_tx.clone()));
+    let consumer = tokio::spawn(consume(rx, state.clone(), sse_tx.clone(), signed_in_players.clone()));
     let server = tokio::spawn(server(rx_sig, server_config, Arc::new(app_state_with_sse)));
 
     tokio::select! {
@@ -160,7 +226,12 @@ async fn main() {
     }
 }
 
-async fn consume(mut rx: UnboundedReceiver<Message>, state: Arc<AppState>, sse_tx: broadcast::Sender<SseEvent>) {
+async fn consume(
+    mut rx: UnboundedReceiver<Message>, 
+    state: Arc<AppState>, 
+    sse_tx: broadcast::Sender<SseEvent>,
+    signed_in_players: Arc<TokioRwLock<HashSet<u64>>>,
+) {
     while let Some(msg) = rx.recv().await {
         match msg {
             Message::Disconnect => { break; }
@@ -201,6 +272,75 @@ async fn consume(mut rx: UnboundedReceiver<Message>, state: Arc<AppState>, sse_t
                     });
                 }
             }
+            Message::PlayerInsert { id, char, x, z } => {
+                // Check if this player is actually signed in
+                if signed_in_players.read().await.contains(&id) {
+                    if let Some(player) = state.player.get(char) {
+                        player.nodes.write().await.insert(id, [x, z]);
+                        // Send SSE event for player insert with entity ID
+                        let _ = sse_tx.send(SseEvent {
+                            message: format!("player_insert:{}:{}", char, id),
+                        });
+                    } else {
+                        eprintln!("Warning: Received PlayerInsert for unknown character {}", char);
+                    }
+                } else {
+                    // This is not a signed-in player, ignore the event
+                    // (Could be an offline player or other mobile entity)
+                }
+            }
+            Message::PlayerDelete { id, char } => {
+                // For deletes, we'll process them regardless of signed-in status
+                // since the player might have just signed out
+                if let Some(player) = state.player.get(char) {
+                    if player.nodes.write().await.remove(id).is_some() {
+                        // Send SSE event for player delete with entity ID
+                        let _ = sse_tx.send(SseEvent {
+                            message: format!("player_delete:{}:{}", char, id),
+                        });
+                    } else {
+                        eprintln!("Warning: Tried to remove non-existent player entity {}", id);
+                    }
+                } else {
+                    eprintln!("Warning: Received PlayerDelete for unknown character {}", char);
+                }
+            }
+            Message::PlayerSignIn { id, username } => {
+                signed_in_players.write().await.insert(id);
+                
+                // If we have a username, store it in all player groups
+                if let Some(username) = username {
+                    for (_, player_group) in state.player.iter() {
+                        player_group.player_names.write().await.insert(id, username.clone());
+                    }
+                }
+            }
+            Message::PlayerSignOut { id } => {
+                signed_in_players.write().await.remove(&id);
+                
+                // Remove the player's location data from all player groups
+                for (char_id, player_group) in state.player.iter() {
+                    if player_group.nodes.write().await.remove(id).is_some() {
+                        // Also remove the player name
+                        player_group.player_names.write().await.remove(&id);
+                        // Send SSE event for player removal with entity ID
+                        let _ = sse_tx.send(SseEvent {
+                            message: format!("player_delete:{}:{}", char_id, id),
+                        });
+                    }
+                }
+            }
+            Message::PlayerNameUpdate { id, username } => {
+                // Update the player name in all player groups, regardless of whether they have location data yet
+                // This ensures names are available when players sign in before moving
+                for (_, player_group) in state.player.iter() {
+                    // Store the username for this player in all player groups
+                    // We'll filter by signed-in status when serving API responses
+                    if signed_in_players.read().await.contains(&id) {
+                        player_group.player_names.write().await.insert(id, username.clone());
+                    }
+                }
+            }
         }
     }
 }
@@ -209,10 +349,12 @@ async fn server(rx: oneshot::Receiver<()>, config: ServerConfig, state: Arc<AppS
     let mut app = Router::new()
         .route("/resource/{id}", get(route_resource_id))
         .route("/enemy/{id}", get(route_enemy_id))
+        .route("/player/{id}", get(route_player_id))
         .route("/events", get(route_sse_events))
         .route("/health", get(route_health))
         .route("/resources", get(route_resources))
         .route("/enemies", get(route_enemies))
+        .route("/players", get(route_players))
         .layer(CompressionLayer::new().gzip(true).zstd(true))
         .with_state(state);
 
@@ -231,7 +373,11 @@ async fn server(rx: oneshot::Receiver<()>, config: ServerConfig, state: Arc<AppS
     println!("server listening on {}", addr);
 
     axum::serve(listener, app)
-        .with_graceful_shutdown(async { rx.await.unwrap(); })
+        .with_graceful_shutdown(async { 
+            if let Err(e) = rx.await {
+                eprintln!("Shutdown signal error: {:?}", e);
+            }
+        })
         .await
         .unwrap();
 }
@@ -246,6 +392,12 @@ async fn route_enemies(
     state: State<Arc<AppStateWithSse>>,
 ) -> Json<Value> {
     Json(serde_json::json!(state.app_state.enemies_list))
+}
+
+async fn route_players(
+    state: State<Arc<AppStateWithSse>>,
+) -> Json<Value> {
+    Json(serde_json::json!(state.app_state.players_list))
 }
 
 async fn route_health() -> Json<Value> {
@@ -305,17 +457,64 @@ async fn route_enemy_id(
     let Some(enemy) = state.app_state.enemy.get(id) else {
         return Err((StatusCode::NOT_FOUND, format!("Enemy ID not found: {}", id)))
     };
-    let nodes = enemy.nodes.read().await
+    let nodes = enemy.nodes.read().await;
+    
+    // Keep MultiPoint format for enemies (unlike players which use individual Point features)
+    let coordinates: Vec<[f64; 2]> = nodes
         .values()
-        .map(|row| row.map(|e| e as f64 / 1_000f64))
-        .collect::<Vec<_>>();
+        .map(|coords| [coords[0] as f64 / 1_000f64, coords[1] as f64 / 1_000f64])
+        .collect();
 
     Ok(Json(serde_json::json!({
         "type": "FeatureCollection",
-        "features": [{
-            "type": "Feature",
-            "properties": enemy.properties,
-            "geometry": { "type": "MultiPoint", "coordinates": nodes }
-        }]
+        "features": [
+            {
+                "type": "Feature",
+                "properties": enemy.properties,
+                "geometry": {
+                    "type": "MultiPoint",
+                    "coordinates": coordinates
+                }
+            }
+        ]
+    })))
+}
+
+async fn route_player_id(
+    Path(id): Path<i32>,
+    state: State<Arc<AppStateWithSse>>,
+) -> Result<Json<Value>, (StatusCode, String)> {
+    let Some(player) = state.app_state.player.get(id) else {
+        return Err((StatusCode::NOT_FOUND, format!("Player ID not found: {}", id)))
+    };
+    let nodes = player.nodes.read().await;
+    let player_names = player.player_names.read().await;
+    
+    // Create individual Point features with entity IDs and player names in properties
+    let features: Vec<serde_json::Value> = nodes
+        .iter()
+        .map(|(entity_id, coords)| {
+            let player_name = player_names.get(&entity_id)
+                .cloned()
+                .unwrap_or_else(|| format!("Player_{}", entity_id));
+            
+            serde_json::json!({
+                "type": "Feature",
+                "properties": {
+                    "entity_id": entity_id,
+                    "player_name": player_name,
+                    "makeCanvas": player.properties.get("makeCanvas").unwrap_or(&serde_json::json!("10"))
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [coords[0] as f64 / 1_000f64, coords[1] as f64 / 1_000f64]
+                }
+            })
+        })
+        .collect();
+
+    Ok(Json(serde_json::json!({
+        "type": "FeatureCollection",
+        "features": features
     })))
 }
